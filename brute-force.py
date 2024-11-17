5c7b064edd410d570c5e9da493e7dbe6ea8135b6
Marcelo Dantas <marcelofcd@gmail.com>
2024-11-16 23:53:13 -0500

    initial checking files name done

--- brute-force.py
+++ brute-force.py
@@ -5,10 +5,15 @@ import sys
 def checking_command():
     command = sys.argv[1::]
     command.split(' ')
+    if '-u' in command:
+        index = command.index('-u')
+        uname_file = command[index + 1]
+    if '-p' in command:
+        index = command.index('-p')
+        passwd_file = command[index + 1]
+    uname = reading_files(uname_file)
+    passwd = reading_files(passwd_file)
 
-    uname = reading_files()
-    passwd = reading_files()
-    
 
 def reading_files():
     with 
