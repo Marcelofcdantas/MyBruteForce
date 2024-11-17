83b444d672d6ff3a1865dad813c64aa951c30f25
Marcelo Dantas <marcelofcd@gmail.com>
2024-11-17 00:19:48 -0500

    core done need fix reading files

--- brute-force.py
+++ brute-force.py
@@ -4,20 +4,31 @@ import sys
 
 def checking_command():
     command = sys.argv[1::]
-    command.split(' ')
+    url = sys.argv[1]
+    if not url.startswith(('http://', 'https://')):
+        url = 'http://' + url
+    print(url)
     if '-u' in command:
         index = command.index('-u')
         uname_file = command[index + 1]
     if '-p' in command:
         index = command.index('-p')
         passwd_file = command[index + 1]
+    print(uname_file)
+    print(passwd_file)
     uname = reading_files(uname_file)
     passwd = reading_files(passwd_file)
+    for names in uname:
+        for passwords in passwd:
+            connecting_to_site(url, uname, passwd)
 
-
-def reading_files():
-    with 
-
+def reading_files(file):
+    texts = []
+    with open(file, 'r'):
+        for line in file:
+            line = line.strip()
+            texts.append(line)
+        return texts
 
 def connecting_to_site(url, username, passwd):
     login_data = {
@@ -26,7 +37,7 @@ def connecting_to_site(url, username, passwd):
     }
     response = requests.post(url, data=login_data)
 
-    if response.status_code(200):
+    if response.status_code == 200:
         print(f'{username} and {passwd} found')
 
 
